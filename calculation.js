0eaefe4dc394962867e7c60b75b3c3f0a5f4532d
Dario Passariello <dariopassariello@gmail.com>
2021-02-16 20:49:55 -0800

    Update calculation.js

--- calculation.js
+++ calculation.js
@@ -1,13 +1,13 @@
+// Copyright by Dario Passariello 2021
+
 function solution(A) {
     var n = '';
     var r = '';
     for( var i=1; i < A.length; ++i ){
         n = ( A.includes( i ) );
         
-          if( n === false ){
-            if( n !== undefined ){
-                r = i;
-            }         
+          if( n === false && n !== undefined ){
+                r = i;      
           }
                 
     }
@@ -19,6 +19,6 @@ function solution(A) {
      }
 }
 
-console.log( solution([1,2,3,4,6,7]) );
-console.log( solution([1,2,3]) );
-console.log( solution([-1,-2]) );
+console.log( solution([1,2,3,4,6,7]) );     // return 5
+console.log( solution([1,2,3]) );           // return 4
+console.log( solution([-1,-2]) );           // return 1
